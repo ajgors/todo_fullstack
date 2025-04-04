2329786087044c813fbe4ebb60cf550db51c8166
ajgors <igor.stadnicki@gmail.com>
2025-04-04 16:16:15 +0200

    add users table

--- backend/sql/db.sql
+++ backend/sql/db.sql
@@ -1,7 +1,17 @@
-CREATE TABLE todo (
+-- drop all tables
+DROP TABLE IF EXISTS todos;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
   id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+  username TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE todos (
+  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+  user_id UUID REFERENCES users(id) ON DELETE CASCADE,
   title TEXT NOT NULL,
   context TEXT NOT NULL,
   checked BOOLEAN DEFAULT FALSE
 );
-
