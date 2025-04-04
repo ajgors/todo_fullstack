5512ac54a6f96fb17a5c062c7c6346779713b097
ajgors <igor.stadnicki@gmail.com>
2025-04-04 16:23:15 +0200

    add users inserts

--- backend/sql/insert.sql
+++ backend/sql/insert.sql
@@ -1,2 +1,5 @@
-INSERT INTO todo (title, context, checked) VALUES ('todo1', 'posprzatac', false)
-INSERT INTO todo (title, context, checked) VALUES ('todo2', 'wysrac', false)
+INSERT INTO users (username, password) VALUES ('admin', 'admin');
+INSERT INTO users (username, password) VALUES ('user', 'user');
+
+INSERT INTO todos (user_id, title, context, checked) VALUES ((SELECT id from users where username ='admin'), 'todo1', 'posprzatac', false);
+INSERT INTO todos (user_id, title, context, checked) VALUES ((SELECT id from users where username ='user'),'todo2', 'wysrac', false);
