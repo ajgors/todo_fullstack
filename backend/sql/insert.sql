b83e9320cd19662309fdfb42add3f5013ccf4bb7
ajgors <igor.stadnicki@gmail.com>
2025-04-04 18:33:26 +0200

    update insert.sql with user that does not have todos

--- backend/sql/insert.sql
+++ backend/sql/insert.sql
@@ -1,5 +1,6 @@
 INSERT INTO users (username, password) VALUES ('admin', 'admin');
 INSERT INTO users (username, password) VALUES ('user', 'user');
+INSERT INTO users (username, password) VALUES ('user2', 'user2');
 
 INSERT INTO todos (user_id, title, context, checked) VALUES ((SELECT id from users where username ='admin'), 'todo1', 'posprzatac', false);
 INSERT INTO todos (user_id, title, context, checked) VALUES ((SELECT id from users where username ='user'),'todo2', 'wysrac', false);
